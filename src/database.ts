ff7a33d344c687ce12cb2175e622947c43043da5
0xhelton <0xhelton@gmail.com>
2024-05-18 22:36:58 -0300

    :sparkles: feat: add Transactions Table and Migrations

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,9 @@
-import { knex as setupKnex, Knex } from "knex";
+import { Knex, knex as setupKnex } from "knex";
 
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: "./tmp/app.db",
+    filename: "./db/app.db",
   },
   useNullAsDefault: true,
   migrations: {
