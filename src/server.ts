c88baaa91b89c30609266d489ebe993133e3321a
0xhelton <0xhelton@gmail.com>
2024-05-19 23:04:47 -0300

    :package: build: Installed Zod and updated environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
 import fastify from "fastify";
+import crpyto from "node:crypto";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+  const transaction = await knex("transactions")
+    .insert({
+      id: crpyto.randomUUID(),
+      title: "Transação de teste",
+      amount: 1000,
+    })
+    .returning("*");
 
-  return tables;
+  return transaction;
 });
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP Server Running");
 });
